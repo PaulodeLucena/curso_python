5df986857a9de868e1e18cf61376656c3eca0039
Paulo de Lucena Costa Cavalcante <100204181+PaulodeLucena@users.noreply.github.com>
2023-09-03 19:20:13 -0300

    Att 0.2 - and, in, not, or.

---
+++ aula23.py
@@ -0,0 +1,14 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+# senha = input('Senha: ')
+
+# if senha != '123456':
+#     print('Senha incorreta.')
+# else:
+#     print('entrou')
+
+
+print(not True)  # False
+print(not False)  # True
