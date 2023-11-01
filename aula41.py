10fa40f291b24bb76150f3c7a8a553cb2c35554e
Paulo de Lucena Costa Cavalcante <100204181+PaulodeLucena@users.noreply.github.com>
2023-11-01 08:38:35 -0300

    While Else

---
+++ aula41.py
@@ -0,0 +1,17 @@
+''' while/else '''
+
+nome = 'valor qualquer'
+
+i = 0
+
+while i < len(nome):
+    letra = nome[i]
+
+    print(letra)
+    i += 1 
+
+else:
+    print('o else foi executado')
+
+print('fora do while')
+
