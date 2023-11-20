2bd8b4fcc3e064be8e1ce9bb78fb0d11a1ce9aef
Paulo de Lucena Costa Cavalcante <100204181+PaulodeLucena@users.noreply.github.com>
2023-11-20 18:37:43 -0300

    List and List Type

---
+++ aula48.py
@@ -0,0 +1,14 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+
+string = 'ABCDE' # 5 caracteres
+lista =  [123, True, 'Paulo de Lucena', 1.2, []] # ''
+lista[-3] = 'Mayara'
+# print(bool(lista))
+# print(lista, type(lista))
+print(lista[2], type(lista[2]))
